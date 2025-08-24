2ffb5670ab2cacb0361e2caa4f0ee36e4790490c
sharmilapalanivel2011 <sharmilapalanivel2011@gmail.com>
2025-08-24 12:57:30 +0530

    Fix line ending settings

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+    return(
+        <div>
+             <h1 className="text-3xl font-medium">Hello Sharmila!</h1>
+            <p>I help you to manage your activities :</p> 
+            
+        </div>
+    )
+}
+export default Header
---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,35 @@
+import AddTodoForm from "./AddTodoForm"
+import TodoList from "./TodoList"
+import { useState } from "react"
+
+function TodoContainer() {
+
+    const [activityArr,setActivityArr] =useState([
+        {
+            id:1,
+            activity:"Go for a Walk"
+        },
+        {
+            id:2,
+            activity:"Go for a ride"
+        },
+        {
+            id:3,
+            activity:"Go for a sleep"
+        }
+    ])
+    return (
+        <div>
+            <div className="flex gap-5 flex-wrap">
+                <AddTodoForm activityArr={activityArr} setActivityArr={setActivityArr}/>
+
+                <TodoList activityArr={activityArr} setActivityArr={setActivityArr}/>
+            
+
+
+            
+        </div>
+        </div>
+    )
+}
+export default TodoContainer
